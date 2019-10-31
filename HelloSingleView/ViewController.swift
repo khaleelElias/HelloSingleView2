904de55ebdc28579c8463d23a95ff3047601daea
Khaleel <khaleel.13@hotmail.com>
2019-10-31 17:25:39 +0100

    Initial Commit

---
+++ HelloSingleView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloSingleView
+//
+//  Created by Khaleel Elias on 2019-10-31.
+//  Copyright © 2019 khaleel-HelloSingleView. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
